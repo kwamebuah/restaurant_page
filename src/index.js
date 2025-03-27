f66bd6fce6db2ac9aa2806941543f7f6893de72e
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-27 22:21:35 +0000

    Import createContact function into index file
    Add eventlistener for menu nav button.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createHomePage } from "./home.js";
 import { createContact } from "./contact.js";
+import { createMenu } from "./menu.js";
 
 (function startWebsite() {
     createHomePage();
@@ -8,6 +9,6 @@ import { createContact } from "./contact.js";
 
 function createEventlisteners() {
     document.querySelector('#home').addEventListener('click', createHomePage);
-    // document.querySelector('#menu').addEventListener('click', createMenu);
+    document.querySelector('#menu').addEventListener('click', createMenu);
     document.querySelector('#contact').addEventListener('click', createContact);
 }
