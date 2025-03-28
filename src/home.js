4653a5b5c6939dcde24476731d5055a0d4b367f4
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-28 18:47:36 +0000

    Remove restaurant name from homepage

--- src/home.js
+++ src/home.js
@@ -7,13 +7,7 @@ export function createHomePage() {
         mainContent.removeChild(mainContent.firstChild);
     }
 
-    const mainHeading = document.createElement('h1');
-    mainHeading.textContent = 'Gentium';
-
-
     headline.textContent = 'Welcome to Gentium! Here, you will find rich delicacies from around the globe.';
 
-
-    mainContent.appendChild(mainHeading);
     mainContent.appendChild(headline);
 }
