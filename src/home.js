40195944123c59209059e99242d638e68bc842d7
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-28 08:18:52 +0000

    Redesign homepage with new name and hero message

--- src/home.js
+++ src/home.js
@@ -1,29 +1,19 @@
-import pastaSalad from "./images/pasta-salad.webp";
-
 export function createHomePage() {
     const mainContent = document.querySelector('#content');
-    const homePage = document.createElement('div');
     const headline = document.createElement('p');
-    const fig1 = document.createElement('figure');
-    const saladImg = document.createElement('img');
-    const fig1cap = document.createElement('figcaption');
 
     // Clear previous page
     while (mainContent.firstChild) {
         mainContent.removeChild(mainContent.firstChild);
     }
 
-    headline.textContent = "In too much of a hurry to make dinner? Feeling overwhelmed at having to cook for extended family visiting over the holiday? Drop by with the family at Nes2U and enjoy meals that taste like home.";
+    const mainHeading = document.createElement('h1');
+    mainHeading.textContent = 'Gentium';
 
-    
-    saladImg.src = pastaSalad;
-    saladImg.alt = "Pasta Salad";
-    saladImg.width = 750;
-    saladImg.height = 750;
-    fig1cap.textContent = 'Pasta and Salad';
-    fig1.appendChild(saladImg);
-    fig1.appendChild(fig1cap);
 
+    headline.textContent = 'Welcome to Gentium! Here, you will find rich delicacies from around the globe.';
+
+
+    mainContent.appendChild(mainHeading);
     mainContent.appendChild(headline);
-    mainContent.appendChild(fig1);
 }
